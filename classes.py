6bacbd79a3a8eff4b86ffc05c898555892135f98
bdemin <42113340+bdemin@users.noreply.github.com>
2020-02-28 18:49:12 +0200

    Added rectangle parameterization

--- classes.py
+++ classes.py
@@ -23,16 +23,31 @@ class Ball(object):
 
 
 class Paddle(object):
-    def __init__(self):
-
-        self.width = 1
-        self.position = np.zeros(2)
+    def __init__(self, display, p_area, pos, color):
+        
+        self.P_AREA = p_area
+        self.TMAX = 2 * (self.P_AREA['h'] + self.P_AREA['w'])
 
     def draw(self):
         pass
 
     def move(self):
         pass
+    def get_pos_of_param_rect(self, t):
+        if t >= self.TMAX:
+            t -= self.TMAX
+        elif t < 0:
+            t += self.t_max
+
+        if 0 <= t <= self.P_AREA['w']:
+            return np.array((self.P_AREA['x'] + t, self.P_AREA['y']))
+        elif self.P_AREA['w'] <= t <= self.P_AREA['w'] + self.P_AREA['h']:
+            return np.array((self.P_AREA['x'] + self.P_AREA['w'], self.P_AREA['y'] + t - self.P_AREA['w']))
+        elif self.P_AREA['w'] + self.P_AREA['h'] <= t <= 2*self.P_AREA['w'] + self.P_AREA['h']:
+            return np.array((self.P_AREA['x'] + 2*self.P_AREA['w'] - t + self.P_AREA['h'] , self.P_AREA['y'] + self.P_AREA['h']))
+        # elif 2*self.P_AREA['w'] + self.P_AREA['h'] <= t <= 2*self.P_AREA['w'] + 2*self.P_AREA['h']:
+        elif 2*self.P_AREA['w'] + self.P_AREA['h'] <= t:
+            return np.array((self.P_AREA['x'] , self.P_AREA['y'] - t + 2*self.P_AREA['w'] + 2*self.P_AREA['h']))
 
 
 class Player(object):
